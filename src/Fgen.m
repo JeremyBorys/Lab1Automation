aa71e2418d38a65e2d1e008566eb256095f04d58
Jeremy Borys <jnickborys@gmail.com>
2013-10-20 08:23:01 -0700

    Added Changes that group1 made to the Fgen
    + now printing error messages instead of leaving -1 as before
    + added basic info/descriptions
    + added setter getter methods

--- src/Fgen.m
+++ src/Fgen.m
@@ -1,26 +1,95 @@
+%Description:
+%Examples:
+%Authors
+
 classdef Fgen
-    %FGEN Summary of this class goes here
-    %   Detailed explanation goes here
-    
+    % these properties are changable by the user
     properties
-        frequency = 1
-        amplitude = 1
-        voltageOffset = 1
+        frequency = -1;
+        amplitude = -1;
+        voltOffset = -1;
+        BaudeRate = -1;
+    end
+    
+    %private properties are only accessible by calling getter and setters
+    properties(access=private)
+        com = -1
         sweepSetting = 1
     end
     
     methods
-        function err = testBaudRate(obj)
-            err = -1
+        
+        function fgen = Fgen()
+            
+            % initialize the com object to talk to
+            fgen.com = serial ('COM1');
+            
+            fgen.testBaudRate();
+            
+            fgen.frequency = getFrequency();
+            fgen.amplitude = getVoltAmplitude();
+            fgen.voltOffset = getVoltOffset();
+            
+        end 
+        
+        % Sets the function generator voltage
+        function setFGenVolt (obj, voltage)
+            
+            %need a condition to check invalid voltages.           
+            %AMPL + (2*OFFSET) <= 10
+            fopen(obj.com);
+            cmd = ':AMPL:VOLT ';
+            cmd = [cmd num2str(voltage)];
+            fprintf(obj.com, cmd);
+            fclose(obj.com);
         end
+        
+        function retVal = getVoltAmplitude(obj)
+            fopen(obj.com);
+            cmd = [' :AMPL:VOLT? '];
+            fprintf(obj.com, cmd);
+            retVal = str2num(fscanf(obj.com));
+            fclose(obj.com);
+        end
+        
+        function testBaudRate(obj)
+            disp('Function: testBaudRate not implemented yet. Please Implement.')
+        end
+        
         function err = sweepFrequency(obj)
-            err = -1
+            disp('Function: sweepFrequency not implemented yet. Please Implement.')
         end
-        function err = getFrequency(obj)
-            err = -1
+        
+        function setFrequency(obj, frequency)
+            fopen(obj.com);
+            cmd = ':FREQ ';
+            cmd = [cmd num2str(frequency)];
+            fprintf(obj.com, cmd);
+            fclose(obj.com);
         end
-        function err = setFrequency(obj)
-            err = -1
+        
+        function getFrequency(obj)
+            fopen(obj.com);
+            cmd = [' :FREQ? '];
+            fprintf(obj.com, cmd);
+            sample = str2num(fscanf(obj.com));
+            fclose(obj.com);
+        end
+        
+        function setVoltOffset( obj, offset )
+            %AMPL + (2*OFFSET) <= 10
+            fopen(obj.com);
+            cmd = [':OFFSet ' num2str(offset)];
+            fprintf(obj.com, cmd);
+            fclose(obj.com);
+        end
+
+        function getVoltOffset(obj)
+            fopen(obj.com);
+            cmd = [' :OFFSet? '];
+            fprintf(obj.com, cmd);
+            sample = str2num(fscanf(obj.com));
+            fclose(obj.com);
         end
     end
     
